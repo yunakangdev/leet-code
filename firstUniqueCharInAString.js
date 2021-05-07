be0f5886ab2b5531612a30c89530a4cf2363373d
Jean Kang <jeankang.dev@gmail.com>
2021-05-07 12:00:58 -0400

    firstUniqueCharInAString: better time & memory complexity

--- firstUniqueCharInAString.js
+++ firstUniqueCharInAString.js
@@ -2,14 +2,10 @@
 
 function firstUniqChar(s) {
 
-	s = s.split('');
-
 	for (let i = 0; i < s.length; i++) {
-		let duplicateCounter = 0;
-		for (let j = i + 1; j < s.length; j++) {
-			if (s[j] === s[i]) duplicateCounter++;
-		}
-		if (duplicateCounter === 0) return s[i];
+        if (s.indexOf(s[i]) === i && s.lastIndexOf(s[i]) === i) {
+            return i;
+        }
 	}
 
   return -1;
