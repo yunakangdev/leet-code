4ef3d93897ede8edd4afdf9b472f60d15bc0bfc6
Jean Kang <jeankang.dev@gmail.com>
2021-05-09 11:54:41 -0400

    validPalindrome: better time & memory complexity

--- validPalindrome.js
+++ validPalindrome.js
@@ -1,32 +1,41 @@
 'use strict';
 
 const isPalindrome = function(s) {
-  // replace non-alphanumeric chars with '' and convert it to lower case
   s = s.replace(/[^a-z0-9]/gi,'').toLowerCase();
+  let sArray = s.split('');
+  let sArrayReversed = s.split('').reverse();
+  
+  if (JSON.stringify(sArray) === JSON.stringify(sArrayReversed)) return true
+  else return false;
+};
+
+// 'use strict';
+
+// const isPalindrome = function(s) {
+//   // replace non-alphanumeric chars with '' and convert it to lower case
+//   s = s.replace(/[^a-z0-9]/gi,'').toLowerCase();
     
-  // divide the string into 2 (front & end)
-  let n = s.length / 2;
-  let front, end;
-
-
-  // if the string is odd number, ignore the char in the center when dividing the char into 2 (front & end)
-  if (!n % 1) {
-    front = s.slice(0, Math.floor(n));
-    end = s.slice(Math.ceil(n));
-  } else {
-    front = s.slice(0, n);
-    end = s.slice(n);
-  }
-
+//   // divide the string into 2 (front & end)
+//   let n = s.length / 2;
+//   let front, end;
+  
+//   // if the string is odd number, ignore the char in the center when dividing the char into 2 (front & end)
+//   if (!n % 1) {
+//     front = s.slice(0, Math.floor(n));
+//     end = s.slice(Math.ceil(n));
+//   } else {
+//     front = s.slice(0, n);
+//     end = s.slice(n);
+//   }
     
-  // loop through front & end strings to check if it's panlindrome
-  for (let i = 0; i < front.length; i++) {
-      console.log(`front[${i}]: ${front[i]}`);
-      console.log(`end[end.length - ${i} + 1]: ${end[end.length - (i + 1)]}`);
-    if (front[i] !== end[end.length - (i + 1)]) return false;
-  }
-    return true;
-  };
+//   // loop through front & end strings to check if it's panlindrome
+//   for (let i = 0; i < front.length; i++) {
+//       console.log(`front[${i}]: ${front[i]}`);
+//       console.log(`end[end.length - ${i} + 1]: ${end[end.length - (i + 1)]}`);
+//     if (front[i] !== end[end.length - (i + 1)]) return false;
+//   }
+//     return true;
+//   };
 
-  // let s = "A man, a plan, a canal: Panama";
-  // console.log(isPalindrome(s));
+//   // let s = "A man, a plan, a canal: Panama";
+//   // console.log(isPalindrome(s));
