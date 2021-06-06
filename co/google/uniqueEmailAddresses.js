9f6af958737d882fcda5aa1b784b76033668701a
Jean Kang <jeankang.dev@gmail.com>
2021-06-06 12:51:48 -0400

    uniqueEmailAddresses: better way

--- co/google/uniqueEmailAddresses.js
+++ co/google/uniqueEmailAddresses.js
@@ -1,30 +1,68 @@
 'use strict';
 
-// Every valid email consists of a local name and a domain name, separated by the '@' sign. Besides lowercase letters, the email may contain one or more '.' or '+'.
+// localPart@domainPart
+// considering below 2 rules, return the number of unique email addresses from the emails array
+// . in the localPart will be ignored
+// characters after + in the local part will be ignored
 
-// For example, in "alice@leetcode.com", "alice" is the local name, and "leetcode.com" is the domain name.
-// If you add periods '.' between some characters in the local name part of an email address, mail sent there will be forwarded to the same address without dots in the local name. Note that this rule does not apply to domain names.
+// new way - better
 
-// For example, "alice.z@leetcode.com" and "alicez@leetcode.com" forward to the same email address.
-// If you add a plus '+' in the local name, everything after the first plus sign will be ignored. This allows certain emails to be filtered. Note that this rule does not apply to domain names.
+function numUniqueEmails(emails) {
 
-// For example, "m.y+name@email.com" will be forwarded to "my@email.com".
-// It is possible to use both of these rules at the same time.
+  // make a new Set variable to store unique values
+  const set = new Set();
+  
+  // normalize each emails in for loops using a separate function and add the new email to the set
+  for (let email in emails) {
+    const newEmail = normalizeEmail(email);
+    set.add(newEmail);
+  }
 
-// Given an array of strings emails where we send one email to each email[i], return the number of different addresses that actually receive mails.
+  // a separate function to normalize the email
+  function normalizeEmail(email) {
+    let [local, domain] = email.split('@');
+    local = local.replace(/(\.)|(\+.*)/g, '');
+    return local + '@' + domain;
+  }
 
-// Example 1:
-// Input: emails = ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
-// Output: 2
-// Explanation: "testemail@leetcode.com" and "testemail@lee.tcode.com" actually receive mails.
-
-let numUniqueEmails = (emails) => {
-    emails.split('');
-    let localPart = emails.split('').map(email => {
-      let result = email.split('@');
-        console.log(result);
-    });
+  // return the size of the set
+  return set.size;
 }
 
 let emails = ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"];
+console.log(numUniqueEmails(emails));
+
+// old way
+function numUniqueEmails(emails) {
+  // extract local parts
+  let localParts = emails.map(email => {
+    let localPart = email.split('@')[0];
+    return localPart.replace(/\./ig, '').split('+')[0];
+  });
+
+  // extract domain parts
+  let domainParts = emails.map(email => {
+    return email.split('@')[1];
+  });
+
+  let newEmails = [];
+
+  // concat & join local parts + @ + domain parts and make a new array
+  for (let i = 0; i < emails.length; i++) {
+    let newEmail = [].concat(localParts[i], '@', domainParts[i]).join('');
+    newEmails.push(newEmail);
+  }
+
+  function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
+  }
+
+  // filter unique emails
+  let uniqueEmails = newEmails.filter(onlyUnique);
+
+  return uniqueEmails.length;
+}
+
+let emails = ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"];
+
 console.log(numUniqueEmails(emails));
