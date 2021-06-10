d46a2c79ccae94e3de0d41b23e37231978a25235
Jean Kang <jeankang.dev@gmail.com>
2021-06-10 00:30:55 -0400

    removeDuplicates: another way without making an extra space

--- removeDuplicatesFromSortedArray.js
+++ removeDuplicatesFromSortedArray.js
@@ -1,2 +1,30 @@
 'use strict';
 
+const removeDuplicates = function(nums) {
+  // make a function that returns a unique value
+  function unique(num, index) {
+    return nums.indexOf(num) === index;
+  }
+
+  // filter the array with unique values using a separate function
+  nums = nums.filter(unique);
+
+  // return the length of the array
+  return nums.length;
+}
+
+// const removeDuplicates = function(nums) {
+// 	// make a Set to store unique values
+// 	let set = new Set();
+
+// 	// add each element of the array to the Set
+// 	for (let num of nums) {
+// 		set.add(num);
+// 	}
+	
+// 	// return the size of the Set
+// 	return set.size;
+// }
+
+let nums = [0,0,1,1,1,2,2,3,3,4];
+console.log(removeDuplicates(nums));
